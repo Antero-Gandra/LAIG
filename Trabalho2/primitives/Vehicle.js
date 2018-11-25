48ff587fd03a8016e1dcd753ed3fcb9de4c8a08c
Dozed12 <ftmmsm@sapo.pt>
2018-11-25 16:39:07 +0000

    Use Patch in Vehicle definition

--- Trabalho2/primitives/Vehicle.js
+++ Trabalho2/primitives/Vehicle.js
@@ -6,60 +6,40 @@ class Vehicle extends CGFobject {
         this.scene = scene;
 
         this.center = new Cylinder2(this.scene, 2, 0, 3, 20, 20);
-        this.back = new Circle(this.scene,20);
+        this.back = new Circle(this.scene, 20);
 
         //Wing upper side
-        this.wingSurfaceUp = new CGFnurbsSurface(4,1,
-            [
-                [
-                    [3, -.2, 0, 1],
-                    [3, 1, 0, 1]
-                ],
-                [
-                    [1, -1, -.5, 1],
-                    [1, 1, -.5, 1]
-                ],
-                [
-                    [0, -1, -.5, 1],
-                    [0, 1, -.5, 1]
-                ],
-                [
-                    [-1, -1, -.5, 1],
-                    [-1, 1, -.5, 1]
-                ],
-                [
-                    [-3, -.2, 0, 1],
-                    [-3, 1, 0, 1]
-                ]
-            ]);
-        this.wingObjUp = new CGFnurbsObject(this.scene, 20, 20, this.wingSurfaceUp);
+        this.wingSurfaceUp = [
+            { x: 3, y: -.2, z: 0 },
+            { x: 3, y: 1, z: 0 },
+            { x: 1, y: -1, z: -.5 },
+            { x: 1, y: 1, z: -.5 },
+            { x: 0, y: -1, z: -.5 },
+            { x: 0, y: 1, z: -.5 },
+            { x: -1, y: -1, z: -.5 },
+            { x: -1, y: 1, z: -.5 },
+            { x: -3, y: -.2, z: 0 },
+            { x: -3, y: 1, z: 0 }
+        ];
+
+        this.wingObjUp = new Patch(this.scene, 5, 2, this.wingSurfaceUp, 20, 20);
 
         //Wing lower side (like upper but order of points reversed)
-        this.wingSurfaceDown = new CGFnurbsSurface(4,1,
-            [
-                [
-                    [-3, -.2, 0, 1],
-                    [-3, 1, 0, 1]
-                ],
-                [
-                    [-1, -1, -.5, 1],
-                    [-1, 1, -.5, 1]
-                ],
-                [
-                    [0, -1, -.5, 1],
-                    [0, 1, -.5, 1]
-                ],
-                [
-                    [1, -1, -.5, 1],
-                    [1, 1, -.5, 1]
-                ],
-                [
-                    [3, -.2, 0, 1],
-                    [3, 1, 0, 1]
-                ]
-            ]);
-        this.wingObjDown = new CGFnurbsObject(this.scene, 20, 20, this.wingSurfaceDown);
-        
+        this.wingSurfaceDown = [
+            { x: -3, y: -.2, z: 0 },
+            { x: -3, y: 1, z: 0 },
+            { x: -1, y: -1, z: -.5 },
+            { x: -1, y: 1, z: -.5 },
+            { x: 0, y: -1, z: -.5 },
+            { x: 0, y: 1, z: -.5 },
+            { x: 1, y: -1, z: -.5 },
+            { x: 1, y: 1, z: -.5 },
+            { x: 3, y: -.2, z: 0 },
+            { x: 3, y: 1, z: 0 }
+        ];
+
+        this.wingObjDown = new Patch(this.scene, 5, 2, this.wingSurfaceDown, 20, 20);
+
 
     };
 
@@ -67,24 +47,24 @@ class Vehicle extends CGFobject {
 
         this.scene.pushMatrix();
 
-            this.scene.rotate(-Math.PI/2, 1, 0, 0);
-            this.scene.rotate(Math.PI, 0, 0, 1);
-            this.scene.scale(1,1,.4);
-            
-            this.center.display();
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(Math.PI, 0, 0, 1);
+        this.scene.scale(1, 1, .4);
 
-            this.scene.pushMatrix();       
-                this.scene.rotate(Math.PI, 1, 0,0);     
-                this.wingObjUp.display();
-                this.wingObjDown.display();
-            this.scene.popMatrix();
+        this.center.display();
 
-            this.scene.pushMatrix();
-                this.scene.translate(0, -3, 0);
-                this.scene.rotate(Math.PI/2, 1, 0,0);
-                this.scene.scale(2.05, 2.05, 1);
-                this.back.display();
-            this.scene.popMatrix();
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI, 1, 0, 0);
+        this.wingObjUp.display();
+        this.wingObjDown.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, -3, 0);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.scale(2.05, 2.05, 1);
+        this.back.display();
+        this.scene.popMatrix();
 
         this.scene.popMatrix();
     }
