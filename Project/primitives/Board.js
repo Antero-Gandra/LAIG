834ba87f8eaf2e56663f3f7e177cab0d4b1c0c9f
Dozed12 <up201607929@fe.up.pt>
2018-12-28 23:07:41 +0000

    Board tiles display

--- Project/primitives/Board.js
+++ Project/primitives/Board.js
@@ -5,10 +5,46 @@ class Board extends CGFobject {
         super(scene);
         this.scene = scene;
 
+        this.size = 8;
+
+        //Setup material
+        this.appearance = new CGFappearance(this.scene);
+        this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
+        this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
+        this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
+        this.appearance.setShininess(120);
+
+        //Data
+        this.matrix = [];
+        for (var i = 0; i < this.size; i++)
+            this.matrix[i] = new Array(this.size);
+
+        //Places
+        this.tile = new Rectangle(scene, -1, -1, 1, 1);
+
     };
 
     display() {
 
+        //Use hardcoded tile texture
+        if (this.appearance.texture == null) {
+            if (this.scene.graph.loadedTextures.length >= 2)
+                this.appearance.setTexture(this.scene.graph.loadedTextures[0].tex);
+        }
+        this.appearance.apply();
+
+        //Display Tiles
+        this.scene.pushMatrix();
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.translate(-2 * this.size / 2 - 1, 2 * this.size / 2 - 1, 0);
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                this.scene.translate(2, 0, 0);
+                this.tile.display();
+            }
+            this.scene.translate(-2 * this.size, -2, 0);
+        }
+        this.scene.popMatrix();
     }
 
 }
